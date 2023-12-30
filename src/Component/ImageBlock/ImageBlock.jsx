e14a2c290c90d1a842b35f3747146bf4d40504c8
rishabhsuyal <rishabhsuyal2001@gmail.com>
2023-12-30 22:46:16 +0530

    Homepage Changes

--- src/Component/ImageBlock/ImageBlock.jsx
+++ src/Component/ImageBlock/ImageBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ImageBlock({url,name,position}) {
   return (
-    <div className='w-56 border-2 p-1'> 
+    <div className='w-56 border-2 p-1 bg-white'> 
         <img src={url} alt='udip'/>
         <h1 className='text-rose-500 font-bold text-center'>{name}</h1>
         <h3 className='text-center font-mono'>{position}</h3>
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -5,12 +5,17 @@ import CCM from "../../Images/ccm.jpg"
 
 function Homepage() {
   return (
-    <div className='bg-[url(https://i.pinimg.com/564x/ff/5a/a9/ff5aa9f0387f70a6bf710caeb45ee699.jpg)] bg-cover h-[75vh]'>
-    <h1 className='text-3xl text-center font-mono p-10  font-bold'>State Tubewell Automation System</h1>
+    <div className=''>
+
+      <div className='bg-[url(https://www.newsclick.in/sites/default/files/styles/responsive_885/public/2022-11/tubewell.jpg?itok=QrKB9_ye)] h-32 flex justify-center items-center mb-5'>
+        <div className='bg-yellow-400 p-3'>
+          <h1 className='text-3xl text-white font-serif font-bold'>State Tubewell Automation System</h1>
+        </div>
+      </div>
       <div className='flex gap-5 justify-center' >
         <ImageBlock url={CM} name={"Yogi Adityanath"} position={"Hon'ble Chief Minister Uttar Pradesh"} />
         <ImageBlock url={CCM} name={"Shri Swatantra Dev Singh"} position={"Hon'ble Cabinet Minister Jal Shakti, Uttar Pradesh"} />
-    </div>
+      </div>
     </div>
   )
 }
